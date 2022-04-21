130568cada49a43e5d6c4bc82412b59ad1ffff49
Jhones Henrique <jhoneshenriquedasilva@gmail.com>
2022-04-21 11:05:26 -0300

    Project componenet - creating component and retrieving data from db according to id

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Container from "./components/layout/Container";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Projects from "./components/pages/Projects";
+import Project from "./components/pages/Project";
 
 function App() {
   return (
@@ -15,10 +16,11 @@ function App() {
        <Container customClass="min-height">
           <Routes>
             <Route exact path="/" element={ <Home />}></Route>
-            <Route exact path="/company" element={ <Company />}></Route>
-            <Route exact path="/projects" element={ <Projects />}></Route>
-            <Route exact path="/contact" element={ <Contact />}></Route>
-            <Route exact path="/newproject" element={ <NewProject />}></Route>
+            <Route  path="/company" element={ <Company />}></Route>
+            <Route  path="/projects" element={ <Projects />}></Route>
+            <Route  path="/contact" element={ <Contact />}></Route>
+            <Route  path="/newproject" element={ <NewProject />}></Route>
+            <Route  path="/project/:id" element={ <Project />}></Route>
           </Routes>
        </Container>
        <Footer />
